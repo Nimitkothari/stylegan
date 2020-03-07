87f7293eef34ef62ed0624dc163e3557750d364a
Nimitkothari <kotharinimit8@gmail.com>
2020-03-08 00:52:55 +0530

    Update training_loop.py

--- training/training_loop.py
+++ training/training_loop.py
@@ -130,7 +130,8 @@ def training_loop(
     mirror_augment          = False,    # Enable mirror augment?
     drange_net              = [-1,1],   # Dynamic range used when feeding image data to the networks.
     image_snapshot_ticks    = 1,        # How often to export image snapshots?
-    network_snapshot_ticks  = 10,       # How often to export network snapshots?
+    #network_snapshot_ticks  = 10,       # How often to export network snapshots?
+    network_snapshot_ticks  = 5,       # How often to export network snapshots?
     save_tf_graph           = False,    # Include full TensorFlow computation graph in the tfevents file?
     save_weight_histograms  = False,    # Include weight histograms in the tfevents file?
     resume_run_id           = '/content/gdrive/My Drive/sgan/results/00000-sganstylegan-1gpu/',     # Run ID or network pkl to resume training from, None = start from scratch.
